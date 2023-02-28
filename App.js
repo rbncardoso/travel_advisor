44add9db886c50a66e3100dc9bbd2246fcfe83d9
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-02-28 16:02:12 -0100

    added README to the project

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <TailwindProvider>
       <View className="flex-1 items-center justify-center bg-white">
-        <Text>Open up App.js to start working on your app hghh!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     </TailwindProvider>
   );
