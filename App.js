48c622f269428e59b250a05d2506cc4b72a69d23
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-02-28 17:02:58 -0100

    added SafeAreaView from safe-area-context

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
+import { SafeAreaView } from "react-native-safe-area-context";;
+
 
 export default function App() {
   return (
     <TailwindProvider>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <SafeAreaView>
+        <View >
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </SafeAreaView>
     </TailwindProvider>
   );
 }
