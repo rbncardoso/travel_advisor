70ce8066ae6686b1a9478452eaf8b0ced7f1615d
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-03-07 12:40:08 -0100

    added HomeScreen to screns

--- App.js
+++ App.js
@@ -2,16 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { SafeAreaView } from "react-native-safe-area-context";;
-
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <TailwindProvider>
-      <SafeAreaView>
-        <View >
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export class HomeScreen extends Component {
+  render() {
+    return (
+    
+        <View >
+          <Text>Open up Home</Text>
+        </View>
+      
+    )
+  }
+}
+
+export default HomeScreen
