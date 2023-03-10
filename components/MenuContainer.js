09b08673cc8456ea4e3edf95ac4889e63e6d5c95
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-03-10 16:39:32 -0100

    added animation to selected  MenuContainers

--- components/MenuContainer.js
+++ components/MenuContainer.js
@@ -1,20 +1,31 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import * as Animatable  from "react-native-animatable";
+
 
 const MenuContainer = ({ title, imageSrc, type, setType }) => {
   const handlePress = () => {
     setType(title.toLowerCase());
   };
+  const anime = "";
+  const eas = "";
+  const xTime = "";
   return (
     <TouchableOpacity className="items-center justify-center space-y-2 "
       onPress={handlePress}
     >
-      <View className={`w-24 h-24 shadow-sm rounded-full
-       ${type === title.toLowerCase() ? "bg-gray-200 border-b-2 border-l-2 border-r-2 border-[#00BCC9] border-25" : ""}`}>
+      <Animatable.View 
+      className={`w-24 h-24 shadow-sm rounded-full
+      ${type === title.toLowerCase() ? "bg-gray-200 border-t-2 border-l-2 border-r-2 border-[#00BCC9] border-25"  : ""}`}
+      animation={`${type === title.toLowerCase() ? "pulse" : ""}`}
+      easing={`${type === title.toLowerCase() ? "ease-in-out" : ""}`}
+      iterationCount ={`${type === title.toLowerCase() ? "infinite" : ""}`}
+       
+       >
         <Image source={imageSrc}
           className="w-full h-full object-contain"
         />
-      </View>
+      </Animatable.View>
       <Text className="text-[#00BCC9] text-xl font-semibold">{title}</Text>
     </TouchableOpacity>
 
